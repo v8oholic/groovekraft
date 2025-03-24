c7310e9a380a785bd24782daea970f9646746125
v8oholic <steve.powell@outlook.com>
2025-03-24 13:10:47 +0000

    headless mode fixed

--- scraper.py
+++ scraper.py
@@ -1,39 +1,52 @@
+#!/usr/bin/env python3
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
+import sys
+import signal
 
-# Set up headless Chrome options
-chrome_options = Options()
 
-# chrome_options.add_argument("--headless=new")  # Improved headless support
-chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration (macOS-specific issue)
-chrome_options.add_argument("--no-sandbox")  # Bypass sandbox issues
-chrome_options.add_argument("--disable-dev-shm-usage")  # Avoid memory overflow errors
-chrome_options.add_argument("--window-size=1280,1024")  # Ensure screen dimensions
-chrome_options.add_argument("--remote-debugging-port=9222")  # Debugging info
-chrome_options.add_argument(
-    "--disable-features=NetworkService,NetworkServiceInProcess")  # Helps on macOS
-chrome_options.add_argument("--disable-software-rasterizer")  # Force CPU rasterizer
-chrome_options.add_argument("--enable-logging")  # Enable verbose logs
-chrome_options.add_argument("--log-level=0")  # Max log level for diagnostics
-# chrome_options.add_argument("--disable-headless")
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    sys.exit(0)
 
-# Create the service object for ChromeDriver
-# service = Service(ChromeDriverManager().install())
 
-# Initialize the WebDriver with the correct service and options
-# driver = webdriver.Chrome(service=service, options=chrome_options)
+def init_driver():
 
-# Force installation of the closest compatible driver
-service = Service(ChromeDriverManager(driver_version="134.0.6998.165").install())
-driver = webdriver.Chrome(service=service, options=chrome_options)
+    # Set up headless Chrome options
+    chrome_options = Options()
+
+    chrome_options.add_argument("--headless=new")  # Improved headless support
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration (macOS-specific issue)
+    chrome_options.add_argument("--no-sandbox")  # Bypass sandbox issues
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Avoid memory overflow errors
+    chrome_options.add_argument("--window-size=1280,1024")  # Ensure screen dimensions
+    chrome_options.add_argument("--enable-websocket-over-http2")
+    chrome_options.add_argument(
+        "--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.165 Safari/537.36")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option("useAutomationExtension", False)
+    # chrome_options.add_argument("--disable-software-rasterizer")  # Force CPU rasterizer
+    # chrome_options.add_argument("--enable-logging")  # Enable verbose logs
+    # chrome_options.add_argument("--log-level=0")  # Max log level for diagnostics
+
+    # Force installation of the closest compatible driver
+    service = Service(ChromeDriverManager(driver_version="134.0.6998.165").install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    return driver
 
 
 def scrape_table(url):
+
     try:
+        driver = init_driver()
+
         # Open the URL using Selenium
         driver.get(url)
 
@@ -87,11 +100,20 @@ urls = [
     'https://www.discogs.com/master/67913-Snap-Rhythm-Is-A-Dancer'
 ]
 
-# Loop through URLs and scrape tables
-for url in urls:
-    df = scrape_table(url)
-    if df is not None:
-        print(f"Data from {url}:")
-        print(df.head())
-        # Save to CSV if needed
-        df.to_csv(f"output_{url.split('/')[-1]}.csv", index=False)
+
+def main():
+
+    # Loop through URLs and scrape tables
+    for url in urls:
+        df = scrape_table(url)
+        if df is not None:
+            print(f"Data from {url}:")
+            print(df.head())
+            # Save to CSV if needed
+            df.to_csv(f"output_{url.split('/')[-1]}.csv", index=False)
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+
+    main()
