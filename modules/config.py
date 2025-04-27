04768f53cf6fe23bea253cedd82f4ed733635cc2
v8oholic <steve.powell@outlook.com>
2025-04-27 20:50:26 +0100

    remove config file

--- modules/config.py
+++ modules/config.py
@@ -15,19 +15,7 @@ class AppConfig:
     def __init__(self, args, root_folder):
 
         self.verbose = args.verbose
-        self.consumer_key = None
-        self.consumer_secret = None
-        self.oauth_token = None
-        self.oauth_token_secret = None
-        self.username = None
-        self.password = None
-        self.root_folder = root_folder
-        self.app_version = APP_VERSION
-
-    def load_from_config_parser(self, config_parser):
-        self.consumer_key = config_parser.get('Discogs', 'consumer_key', fallback=None)
-        self.consumer_secret = config_parser.get('Discogs', 'consumer_secret', fallback=None)
-        self.oauth_token = config_parser.get('Discogs', 'oauth_token', fallback=None)
-        self.oauth_token_secret = config_parser.get('Discogs', 'oauth_token_secret', fallback=None)
-        self.username = config_parser.get('MusicBrainz', 'username', fallback=None)
-        self.password = config_parser.get('MusicBrainz', 'password', fallback=None)
+        self.discogs_consumer_key = DISCOGS_CONSUMER_KEY
+        self.discogs_consumer_secret = DISCOGS_CONSUMER_SECRET
+        self.user_agent = GROOVEKRAFT_USER_AGENT
+        self.app_version = GROOVEKRAFT_VERSION
